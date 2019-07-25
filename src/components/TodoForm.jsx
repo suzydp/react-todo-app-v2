598fcf5bfde0ca850262909c2f51b586d4a93082
Kaai Suzuki <suzydp@me.com>
2019-07-24 22:36:04 -0700

    Replace Form to add todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class TodoForm extends React.Component {
   render() {
     return (
-      <h2 className="subtitle">Hello from TodoForm</h2>
+      <div>
+        <p className="title">Add your Todo</p>
+        <form id="form">
+          <input type="text" name="todo" placeholder="Phone call to Dan" />
+          <button>Add</button>
+        </form>
+      </div>
     )
   }
 }

224c7d07d8f9394dc6a5ac0e1dc84f57a0b237bf
Kaai Suzuki <suzydp@me.com>
2019-07-25 00:33:05 -0700

    Add dummy todo list

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,8 +3,42 @@ import '../app.scss';
 
 class TodoItem extends React.Component {
   render() {
+    // Add dummyTodos to render in this component
+    const dummyTodos = [
+      {
+        id: 1, 
+        text: "Do some laundry", 
+        isDone: false, 
+        todoClick: ()=>console.log("clicked"),
+      },
+      {
+        id: 2, 
+        text: "Buy Apple at grocery", 
+        isDone: false, 
+        todoClick: ()=>console.log("clicked"),
+      },
+      {
+        id: 3, 
+        text: "Clean up my room", 
+        isDone: true, 
+        todoClick: ()=>console.log("clicked"),
+      },
+    ];
+
     return (
-      <h2 className="subtitle">Hello from TodoItem</h2>
+      <ul>
+        {dummyTodos.map(todo => 
+          <li
+            key={todo.id}
+            onClick={todo.todoClick}
+            style={{
+              textDecoration: todo.completed ? 'line-through' : 'none'
+            }}
+          >
+            {todo.text}
+          </li>
+        )}
+      </ul>
     )
   }
 }

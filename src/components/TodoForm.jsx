185eb8e33584d5df9ab82c646c73412d8cc4d9dd
Kaai Suzuki <suzydp@me.com>
2019-07-24 22:57:28 -0700

    Add state and event handler to Enable form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
 class TodoForm extends React.Component {
+  // init state
+  state = {
+    inputValue: "",
+  }
+
+  // handle form data
+  handleChange = (event) => {
+    // change state by using setState
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+
+  // handle data submitting
+  handleSubmit = (event) => {
+    // To prevent executing unstoppable event, use event.preventDefault
+    event.preventDefault();
+    // Appear result when the data successfully sent
+    console.log(this.state.inputValue);
+  }
+
   render() {
     return (
       <div>
         <p className="title">Add your Todo</p>
         <form id="form">
-          <input type="text" name="todo" placeholder="Phone call to Dan" />
-          <button>Add</button>
+          <input onChange={this.handleChange} type="text" name="todo" placeholder="Phone call to Dan" />
+          <button onClick={this.handleSubmit}>Add</button>
         </form>
       </div>
     )

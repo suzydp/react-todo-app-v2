ef519c65c0fea1e89fe583b76ec21dadcbc6eea5
Kaai Suzuki <suzydp@me.com>
2019-07-24 20:55:02 -0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// Load path module to specify output path
+const path = require('path');
+
+module.exports = {
+  // Entry point for export
+  entry: './src/index.jsx',
+  // Output type
+  mode: 'development',
+  output: {
+    // Specify filename and path for output
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  // Declare environment for devserver
+  devServer: {
+    contentBase: 'public',
+    port: 8888,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack.js', ' ', '.js', '.jsx']
+  }
+};
